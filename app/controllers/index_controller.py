f06d66892fbd61a556a658f9ecbc534db7a56c36
Joseph Vick <joevick@vick80.com>
2022-02-08 10:40:26 -0500

    changed name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Joe"
+        name = "Joe V"
         return render_template('index.html', name=name)
